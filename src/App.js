8e9333a9208077080b86a2b5496616a2cd540f41
valereyna <145428872+valereyna@users.noreply.github.com>
2024-08-08 04:20:27 +0700

    add function
    revise the code so that it can shows more than 10 movies with 'load more' button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import { useState, useEffect } from 'react';
-
 import MovieCard from './MovieCard';
 import SearchIcon from './search.svg';
 import './App.css';
 
-const API_URL = 'http://www.omdbapi.com/?apikey=52495d01';
+const API_URL = `https://www.omdbapi.com/?apikey=52495d01`;
+
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
 
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
+  const searchMovies = async (title, pageNumber) => {
+    const response = await fetch(`${API_URL}&s=${title}&page=${pageNumber}`);
     const data = await response.json();
 
-    setMovies(data.Search);
+    if (data.Search) {
+      setMovies(prevMovies => pageNumber === 1 ? data.Search : [...prevMovies, ...data.Search]);
+      setTotalResults(parseInt(data.totalResults));
+    }
   };
 
   useEffect(() => {
-    searchMovies('Batman');
+    searchMovies('Batman', 1);
   }, []);
+
+  const handleSearch = () => {
+    setPage(1);
+    searchMovies(searchTerm, 1);
+  };
+
+  const loadMore = () => {
+    const nextPage = page + 1;
+    setPage(nextPage);
+    searchMovies(searchTerm || 'Batman', nextPage);
+  };
 
   return (
     <div className='app'>
@@ -29,26 +45,30 @@ const App = () => {
           placeholder='Search for movies'
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyPress={(e) => e.key === 'Enter' && handleSearch()}
         />
         <img
           src={SearchIcon}
           alt='SearchIcon'
-          onClick={() => searchMovies(searchTerm)}
+          onClick={handleSearch}
         />
       </div>
 
-      {movies?.length > 0
-        ? (
-          <div className='container'>
-            {movies.map((movie) => (
-              <MovieCard movie={movie} />
-            ))}
-          </div>
-        ) : (
-          <div className='empty'>
-            <h2>No movies found</h2>
-          </div>
-        )}
+      {movies?.length > 0 ? (
+        <div className='container'>
+          {movies.map((movie, index) => (
+            <MovieCard key={`${movie.imdbID}-${index}`} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className='empty'>
+          <h2>No movies found</h2>
+        </div>
+      )}
+
+      {movies.length > 0 && movies.length < totalResults && (
+        <button className='load-more' onClick={loadMore}>Load More</button>
+      )}
     </div>
   );
 };
